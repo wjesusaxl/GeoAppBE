5ba02e835af9597832839136e245a5410fea53ea
Jesus Rodriguez <wjesus_axl@hotmail.com>
2022-07-02 19:02:09 -0500

    updating target

--- api/settings.py
+++ api/settings.py
@@ -123,8 +123,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'testPy',
-        'USER': 'developer',
-        # 'PASSWORD': 'D3v3l@p3r',
+        # 'USER': 'developer',
+        'PASSWORD': 'D3v3l@p3r',
         'PASSWORD': 'developer',
         'HOST': '127.0.0.1',
         'PORT': '3306'
